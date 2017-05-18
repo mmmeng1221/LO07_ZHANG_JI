74dde17458348209d7a28340a991ba7f51cefb84
mmmeng1221 <meng.zhang@utt.fr>
2017-05-18 14:27:44 +0200

    first commit

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ and open the template in the editor.
     <body>
         <?php
         // put your code here
-        echo("haha");
+        echo("hello world!");
         ?>
     </body>
 </html>
